76fe4ab899b9b44d6431295fbf2ff4dd7ed23257
Nicolas Eeckeloo <neeckeloo@gmail.com>
2012-07-10 13:32:08 +0200

    Add unit tests to AmCharts\Chart\Axis\Value class

---
+++ tests/AmCharts/Chart/Axis/ValueTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AmCharts\Chart\Axis;
+
+class ValueTest extends \PHPUnit_Framework_TestCase
+{   
+    /**
+     * @var Value
+     */
+    protected $axis;
+    
+    public function setUp()
+    {
+        $this->axis = new Value;
+    }
+    
+    public function testSetLabelsEnabled()
+    {
+        $this->axis->setLabelsEnabled(true);
+        $this->assertTrue($this->axis->isLabelsEnabled());
+    }
+    
+    public function testToArray()
+    {
+        $this->assertCount(2, $this->axis->toArray());
+    } 
+}
