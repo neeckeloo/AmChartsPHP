ad3f18e87b9935202bee59f9898b94ed2d8ec4fa
Nicolas Eeckeloo <neeckeloo@gmail.com>
2012-07-23 09:16:03 +0200

    Add column and pie chart example

---
+++ examples/3d-pie-chart/index.php
@@ -0,0 +1,23 @@
+<?php
+require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
+
+use AmCharts\Chart,
+    AmCharts\Graph,
+    AmCharts\Manager as ChartManager;
+
+$manager = ChartManager::getInstance();
+$manager->setAmChartsPath('./amcharts.js');
+
+$pie = new Chart\Pie();
+        
+$dataProvider = Chart\DataProvider\Factory::fromFile(__DIR__ . '/data.xml');
+$pie->setDataProvider($dataProvider);
+
+$pie->setTitleField('country')
+    ->setValueField('value')
+    ->setOutlineColor('#ffffff')
+    ->setOutlineAlpha(80)
+    ->setOutlineThickness(2)
+    ->set3D(30, 15);
+
+echo $pie->render();
