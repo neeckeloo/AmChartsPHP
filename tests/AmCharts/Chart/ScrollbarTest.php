a9be1c05643176f10e1a24fd5e5682f48e694914
Nicolas Eeckeloo <neeckeloo@gmail.com>
2012-08-02 09:55:05 +0200

    Add scrollbar test class

---
+++ tests/AmCharts/Chart/ScrollbarTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * AmChartsPHP
+ * 
+ * @link      http://github.com/neeckeloo/AmChartsPHP
+ * @copyright Copyright (c) 2012 Nicolas Eeckeloo
+ */
+namespace AmCharts\Chart;
+
+class ScrollbarTest extends \PHPUnit_Framework_TestCase
+{   
+    /**
+     * @var Scrollbar
+     */
+    protected $scrollbar;
+    
+    public function setUp()
+    {
+        $this->scrollbar = new Scrollbar;
+    }
+}
