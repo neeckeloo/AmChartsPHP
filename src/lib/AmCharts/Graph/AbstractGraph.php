675fe78c319f9eb63a92c9b33c5c669ae7fff2c6
Nicolas Eeckeloo <neeckeloo@gmail.com>
2012-08-22 14:12:56 +0200

    Improve "toArray" method

--- src/lib/AmCharts/Graph/AbstractGraph.php
+++ src/lib/AmCharts/Graph/AbstractGraph.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * AmChartsPHP
- * 
+ *
  * @link      http://github.com/neeckeloo/AmChartsPHP
  * @copyright Copyright (c) 2012 Nicolas Eeckeloo
  */
@@ -12,102 +12,102 @@ use AmCharts\Chart\Setting;
 abstract class AbstractGraph
 {
     /**
-     * @var string 
+     * @var string
      */
     protected $type;
-    
+
     /**
-     * @var string 
+     * @var string
      */
     protected $title;
-    
+
     /**
      * @var Fields
      */
     protected $fields;
-    
+
     /**
-     * @var string 
+     * @var string
      */
     protected $balloonText = '[[value]]';
-    
+
     /**
      * Opacity of fill. Plural form is used to keep the same property names as our Flex charts'.
      * Flex charts can accept array of numbers to generate gradients.
      * Although you can set array here, only first value of this array will be used.
-     * 
-     * @var Setting\Alpha 
+     *
+     * @var Setting\Alpha
      */
     protected $fillAlphas;
-    
+
     /**
      * Fill color. Will use lineColor if not set.
-     * 
-     * @var array 
+     *
+     * @var array
      */
     protected $fillColors;
-    
+
     /**
-     * @var Setting\Alpha 
+     * @var Setting\Alpha
      */
     protected $lineAlpha;
-    
+
     /**
-     * @var Setting\Color 
+     * @var Setting\Color
      */
     protected $lineColor;
-    
+
     /**
-     * @var integer 
+     * @var integer
      */
     protected $lineThickness;
-    
+
     /**
      * Sets type
-     * 
+     *
      * @param string $type
      * @return AbstractGraph
      */
     public function setType($type)
     {
         $this->type = (string) $type;
-        
+
         return $this;
     }
-    
+
     /**
      * Returns type
-     * 
-     * @return string 
+     *
+     * @return string
      */
     public function getType()
     {
         return $this->type;
     }
-    
+
     /**
      * Sets title
-     * 
+     *
      * @param string $title
      * @return AbstractGraph
      */
     public function setTitle($title)
     {
         $this->title = (string) $title;
-        
+
         return $this;
     }
-    
+
     /**
      * Returns title
-     * 
-     * @return string 
+     *
+     * @return string
      */
     public function getTitle()
     {
         return $this->title;
     }
-        
+
     /**
      * Sets and returns fields
      *
@@ -119,63 +119,63 @@ abstract class AbstractGraph
         if (!isset($this->fields)) {
             $this->fields = new Fields();
         }
-        
+
         $this->fields->setFields($fields);
 
         return $this->fields;
     }
-    
+
     /**
      * Sets balloon text
-     * 
+     *
      * @param string $text
      * @return AbstractGraph
      */
     public function setBalloonText($text)
     {
         $this->balloonText = (string) $text;
-        
+
         return $this;
     }
-    
+
     /**
      * Returns balloon text
-     * 
-     * @return string 
+     *
+     * @return string
      */
     public function getBalloonText()
     {
         return $this->balloonText;
     }
-    
+
     /**
      * Sets fill alphas
-     * 
+     *
      * @param integer $alpha
      * @return AbstractGraph
      */
     public function setFillAlphas($alpha)
     {
         $this->fillAlphas = new Setting\Alpha($alpha);
-        
+
         return $this;
     }
-    
+
     /**
      * Returns fill alphas
-     * 
-     * @return integer 
+     *
+     * @return integer
      */
     public function getFillAlphas()
     {
         return $this->fillAlphas->getOpacity();
     }
-    
+
     /**
      * Sets fill colors
-     * 
+     *
      * @param array|string|Setting\Color $colors
-     * @return AbstractGraph 
+     * @return AbstractGraph
      */
     public function setFillColors($colors)
     {
@@ -183,52 +183,52 @@ abstract class AbstractGraph
             $colors = array($colors);
             $this->setFillColors($colors);
         }
-        
+
         $this->fillColors = array();
         foreach ($colors as $color) {
             if (!($color instanceof Setting\Color)) {
                 $color = new Setting\Color($color);
             }
-            
+
             $this->fillColors[] = $color;
         }
-        
+
         return $this;
     }
-    
+
     /**
      * Returns fill colors
-     * 
-     * @return integer 
+     *
+     * @return integer
      */
     public function getFillColors()
     {
         return $this->fillColors;
     }
-    
+
     /**
      * Sets line alpha
-     * 
-     * @param integer $alpha 
+     *
+     * @param integer $alpha
      * @return AbstractGraph
      */
     public function setLineAlpha($alpha)
     {
         $this->lineAlpha = new Setting\Alpha($alpha);
-        
+
         return $this;
     }
-    
+
     /**
      * Returns line alpha
-     * 
-     * @return integer 
+     *
+     * @return integer
      */
     public function getLineAlpha()
     {
         return $this->lineAlpha->getOpacity();
     }
-    
+
     /**
      * Sets line color
      *
@@ -247,21 +247,21 @@ abstract class AbstractGraph
 
         return $this;
     }
-    
+
     /**
      * Returns line color
-     * 
-     * @return string 
+     *
+     * @return string
      */
     public function getLineColor()
     {
         return $this->lineColor;
     }
-    
+
     /**
      * Sets line thickness
-     * 
-     * @param integer $thickness 
+     *
+     * @param integer $thickness
      * @return AbstractGraph
      */
     public function setLineThickness($thickness)
@@ -269,48 +269,44 @@ abstract class AbstractGraph
         if ($thickness < 0) {
             throw new Exception\InvalidArgumentException('The thickness value must be positive.');
         }
-        
+
         $this->lineThickness = (int) $thickness;
-        
+
         return $this;
     }
-    
+
     /**
      * Returns line thickness
-     * 
-     * @return integer 
+     *
+     * @return integer
      */
     public function getLineThickness()
     {
         return $this->lineThickness;
     }
-    
+
     /**
      * Returns object properties as array
-     * 
-     * @return array 
+     *
+     * @return array
      */
     public function toArray()
     {
-        $options = array(
-            'type'          => $this->type,
-            'balloonText'   => $this->balloonText,
-            'fillColors'    => $this->fillColors,
-            'lineColor'     => $this->lineColor,
-            'lineThickness' => $this->lineThickness,
-            'title'         => $this->title
-        );
-        
+        $options = array();
+
+        $properties = array_keys(get_object_vars($this));
+        foreach ($properties as $name) {
+            if (isset($this->{$name})) {
+                if ($this->{$name} instanceof Setting\Alpha) {
+                    $options[$name] = $this->{$name}->getValue();
+                } else {
+                    $options[$name] = $this->{$name};
+                }
+            }
+        }
+
         $options = $options + $this->fields()->toArray();
-        
-        if (isset($this->fillAlphas)) {
-            $options['fillAlphas'] = $this->fillAlphas->getValue();
-        }
-        
-        if (isset($this->lineAlpha)) {
-            $options['lineAlpha'] = $this->lineAlpha->getValue();
-        }
-        
+
         return $options;
     }
 }
--- tests/AmCharts/Graph/AbstractGraphTest.php
+++ tests/AmCharts/Graph/AbstractGraphTest.php
@@ -1,93 +1,93 @@
 <?php
 /**
  * AmChartsPHP
- * 
+ *
  * @link      http://github.com/neeckeloo/AmChartsPHP
  * @copyright Copyright (c) 2012 Nicolas Eeckeloo
  */
 namespace AmCharts\Graph;
 
 class AbstractGraphTest extends \PHPUnit_Framework_TestCase
-{   
+{
     /**
      * @var AmCharts\Graph\AbstractGraph
      */
     protected $graph;
-    
+
     public function setUp()
     {
         $class = 'AmCharts\Graph\AbstractGraph';
         $this->graph = $this->getMockForAbstractClass($class);
     }
-    
+
     public function testSetTitle()
     {
         $this->graph->setTitle('Foo');
         $this->assertEquals('Foo', $this->graph->getTitle());
     }
-    
+
     public function testSetType()
     {
         $this->graph->setType('Foo');
         $this->assertEquals('Foo', $this->graph->getType());
     }
-    
+
     public function testGetFields()
     {
         $this->assertInstanceOf('AmCharts\Graph\Fields', $this->graph->fields());
     }
-    
+
     public function testSetBalloonText()
     {
         $this->graph->setBalloonText('Foo');
         $this->assertEquals('Foo', $this->graph->getBalloonText());
     }
-    
+
     public function testSetFillAlphas()
     {
-        $this->graph->setFillAlphas(50);        
+        $this->graph->setFillAlphas(50);
         $this->assertEquals(50, $this->graph->getFillAlphas());
     }
-    
+
     public function testSetFillColors()
     {
         $this->graph->setFillColors(array('#ff0000', '#00ff00', '#0000ff'));
-        
+
         $fillColors = $this->graph->getFillColors();
         $this->assertCount(3, $fillColors);
         $this->assertInstanceOf('AmCharts\Chart\Setting\Color', $fillColors[0]);
-        
+
         $this->graph->setFillColors('#ff0000');
         $fillColors = $this->graph->getFillColors();
         $this->assertCount(1, $fillColors);
         $this->assertInstanceOf('AmCharts\Chart\Setting\Color', $fillColors[0]);
     }
-    
+
     public function testSetLineAlpha()
     {
-        $this->graph->setLineAlpha(50);        
+        $this->graph->setLineAlpha(50);
         $this->assertEquals(50, $this->graph->getLineAlpha());
     }
-    
+
     public function testLineColor()
     {
         $color = $this->graph->getLineColor();
         $this->assertNull($color);
-        
+
         $this->graph->setLineColor('#ff0000');
         $this->assertInstanceOf('AmCharts\Chart\Setting\Color', $this->graph->getLineColor());
     }
-    
+
     public function testSetLineThickness()
     {
         $thickness = 20;
         $this->graph->setLineThickness($thickness);
         $this->assertEquals($thickness, $this->graph->getLineThickness());
     }
-    
+
     public function testToArray()
     {
         $options = $this->graph->toArray();
-        $this->assertCount(6, $options);
+        $this->assertCount(1, $options);
     }
 }
