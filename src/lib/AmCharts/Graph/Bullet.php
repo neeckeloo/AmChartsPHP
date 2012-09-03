e05f8104eacdd40974d54d284d267e99c8960e1b
Nicolas Eeckeloo <neeckeloo@gmail.com>
2012-09-03 15:02:22 +0200

    Throws exception when trying to set invalid bullet type

--- src/lib/AmCharts/Graph/Bullet.php
+++ src/lib/AmCharts/Graph/Bullet.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * AmChartsPHP
- * 
+ *
  * @link      http://github.com/neeckeloo/AmChartsPHP
  * @copyright Copyright (c) 2012 Nicolas Eeckeloo
  */
@@ -18,55 +18,70 @@ class Bullet
     const CUSTOM = 'custom';
 
     /**
-     * @var string 
+     * @var string
      */
     protected $type;
-    
+
+    /**
+     * Valid bullet types
+     *
+     * @var array
+     */
+    protected $validTypes = array(
+    	self::NONE, self::ROUND, self::SQUARE, self::TRIANGLE_DOWN,
+    	self::TRIANGLE_UP, self::BUBBLE, self::CUSTOM
+    );
+
     /**
      * Sets type
-     * 
+     *
      * @param string $field
      * @return Bullet
      */
     public function setType($type)
     {
+        if (!in_array($type, $this->validTypes)) {
+            throw new Exception\InvalidArgumentException(sprintf(
+            	'The bullet type "%s" is not valid.',
+            	$type
+            ));
+        }
+
         $this->type = (string) $type;
-        
+
         return $this;
     }
-    
+
     /**
      * Returns type
-     * 
-     * @return string 
+     *
+     * @return string
      */
     public function getType()
     {
         return $this->type;
     }
-    
+
     /**
      * Returns object properties as array
-     * 
-     * @return array 
+     *
+     * @return array
      */
     public function toArray()
     {
         $options = array();
-        
+
+        $excludedKeys = array('validTypes');
+
         $properties = get_object_vars($this);
         foreach ($properties as $key => $value) {
-            if (!$value) {
+            if (!$value || in_array($key, $excludedKeys)) {
                 continue;
-            }
-
-            if ($key == 'type') {
-                $key = 'bullet';
             }
 
             $options[$key] = $value;
         }
-        
+
         return $options;
     }
 }
