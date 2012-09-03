7e2a44054255118ab242ca99865b1e0e112a0483
Nicolas Eeckeloo <neeckeloo@gmail.com>
2012-09-03 16:02:20 +0200

    Add data provider test class

---
+++ tests/AmCharts/Chart/DataProviderTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * AmChartsPHP
+ * 
+ * @link      http://github.com/neeckeloo/AmChartsPHP
+ * @copyright Copyright (c) 2012 Nicolas Eeckeloo
+ */
+namespace AmCharts\Chart;
+
+class DataProviderTest extends \PHPUnit_Framework_TestCase
+{   
+    /**
+     * @var DataProvider
+     */
+    protected $provider;
+    
+    public function setUp()
+    {
+        $this->provider = new DataProvider(array('foo' => 123, 'bar' => 456));
+    }
+    
+    public function testGetData()
+    {
+    	$this->assertTrue(is_array($this->provider->getData()));
+    }
+}
