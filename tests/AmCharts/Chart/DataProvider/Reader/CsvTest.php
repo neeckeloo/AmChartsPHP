126c8e0f7f1e61e4d1281dec70ac5c7951165945
Nicolas Eeckeloo <neeckeloo@gmail.com>
2012-09-04 09:59:38 +0200

    Add composer.lock file

---
+++ tests/AmCharts/Chart/DataProvider/Reader/CsvTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * AmChartsPHP
+ * 
+ * @link      http://github.com/neeckeloo/AmChartsPHP
+ * @copyright Copyright (c) 2012 Nicolas Eeckeloo
+ */
+namespace AmCharts\Chart\DataProvider\Reader;
+
+class XmlTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Xml
+     */
+    protected $object;
+    
+    public function setUp()
+    {
+        $this->object = new Xml;
+    }
+    
+    public function testFromString()
+    {
+        $xml = '<?xml version="1.0" encoding="UTF-8" ?>
+<root>
+    <item>
+        <name>Foo</name>
+        <value>1</value>
+    </item>
+    <item>
+        <name>Bar</name>
+        <value>2</value>
+    </item>
+    <item>
+        <name>Baz</name>
+        <value>3</value>
+    </item>
+</root>';
+        
+        $items = $this->object->fromString($xml);
+        
+        $this->assertCount(3, $items);
+        
+        $this->assertEquals('Foo', $items[0]['name']);
+        $this->assertEquals(1, $items[0]['value']);
+    }
+}
---
+++ tests/AmCharts/Chart/DataProviderTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * AmChartsPHP
+ * 
+ * @link      http://github.com/neeckeloo/AmChartsPHP
+ * @copyright Copyright (c) 2012 Nicolas Eeckeloo
+ */
+namespace AmCharts\Chart;
+
+class CoordinateTest extends \PHPUnit_Framework_TestCase
+{   
+    /**
+     * @var Coordinate
+     */
+    protected $chart;
+    
+    public function setUp()
+    {
+        $this->chart = $this->getMockForAbstractClass('AmCharts\Chart\Coordinate');
+    }
+    
+    public function testSetSequencedAnimation()
+    {        
+        $this->chart->setSequencedAnimation(false);
+        $this->assertFalse($this->chart->isSequencedAnimation());
+    }
+    
+    public function testSetStartAlpha()
+    {
+        $this->assertEquals(100, $this->chart->getStartAlpha());
+        
+        $alpha = 20;
+        $this->chart->setStartAlpha($alpha);
+        $this->assertEquals($alpha, $this->chart->getStartAlpha());
+    }
+    
+    public function testSetStartDuration()
+    {
+        $duration = 20;
+        $this->chart->setStartDuration($duration);
+        $this->assertEquals($duration, $this->chart->getStartDuration());
+    }
+    
+    public function testSetStartEffect()
+    {
+        $effect = Coordinate::EFFECT_ELASTIC;
+        $this->chart->setStartEffect($effect);
+        $this->assertEquals($effect, $this->chart->getStartEffect());
+    }
+    
+    /**
+     * @expectedException AmCharts\Exception\InvalidArgumentException 
+     */
+    public function testSetStartEffectWithInvalidParam()
+    {
+        $this->chart->setStartEffect('foo');
+    }
+    
+    public function testSetUrlTarget()
+    {
+        $url = 'foo';
+        $this->chart->setUrlTarget($url);
+        $this->assertEquals($url, $this->chart->getUrlTarget());
+    }
+    
+    public function testGetValueAxis()
+    {
+        $valueAxis = $this->chart->valueAxis();
+        $this->assertInstanceOf('AmCharts\Chart\Axis\Value', $valueAxis);
+    }
+}
